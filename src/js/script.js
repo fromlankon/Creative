f72e5062b221a776941b1beea3e77b4cea5d1da0
fromlankon <abbasovxl@gmail.com>
2023-08-26 02:20:52 +0400

    version1

---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 0) {
+        document.querySelector("nav").classList.add("bg");
+    } else {
+        document.querySelector("nav").classList.remove("bg");
+    }
+});
+
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 0) {
+        document.querySelector(".navLeft").classList.add("fontColor");
+    } else {
+        document.querySelector(".navLeft").classList.remove("fontColor");
+    }
+});
+
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 0) {
+        document.querySelector(".navColor1").classList.add("fontColor");
+    } else {
+        document.querySelector(".navColor1").classList.remove("fontColor");
+    }
+});
+
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 0) {
+        document.querySelector(".navColor2").classList.add("fontColor");
+    } else {
+        document.querySelector(".navColor2").classList.remove("fontColor");
+    }
+});
+
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 0) {
+        document.querySelector(".navColor3").classList.add("fontColor");
+    } else {
+        document.querySelector(".navColor3").classList.remove("fontColor");
+    }
+});
+
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 0) {
+        document.querySelector(".navColor4").classList.add("fontColor");
+    } else {
+        document.querySelector(".navColor4").classList.remove("fontColor");
+    }
+});
